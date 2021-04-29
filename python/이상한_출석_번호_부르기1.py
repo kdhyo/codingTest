6ef58405f0af4cf06387e85a7d4b675cade356af
hyo <kdhyo98@gmail.com>
2021-04-29 23:58:42 +0900

    이상한_출석_번호_부르기1

---
+++ python/이상한_출석_번호_부르기1.py
@@ -0,0 +1,14 @@
+# https://codeup.kr/problem.php?id=6092
+
+#n = int(input())
+a = list(map(int, input().split()))
+
+d = []
+for i in range(24):
+    d.append(0)
+
+for i in range(len(a)):
+    d[a[i]] += 1
+
+for i in range(1, 24):
+    print(d[i], end=' ')
