e9045926afbefafebd02a46b8b2f42420da89ea3
hyo <kdhyo98@gmail.com>
2021-05-05 17:01:10 +0900

    01타일

---
+++ python/백준/01타일.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/1904
+
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+n = int(input())
+
+for k in range(3, n+1):
+    dp[k] = (dp[k-1] + dp[k-2]) % 15746
+
+print(dp[n])
