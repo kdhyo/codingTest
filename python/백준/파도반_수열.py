b400857ed333f4262ad1b23a8f06ff3d9d49fe20
hyo <kdhyo98@gmail.com>
2021-05-07 00:14:07 +0900

    파도반 수열완료

--- python/백준/파도반_수열.py
+++ python/백준/파도반_수열.py
@@ -1,11 +1,11 @@
 n = int(input())
-p = [0 for _ in range(100)]
+max = 101
+p = [1, 1, 1, 2, 2, 3, 4, 5, 7, 9, 12]
+
+for i in range(11, max):
+    p.append(p[i-1] + p[i-5])
 
 
-def p(num):
-
-
-print(p)
 for i in range(n):
-    num = int(input)
-    print(p(num))
+    num = int(input())
+    print(p[num-1])
