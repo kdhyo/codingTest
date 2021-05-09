2d1949ca84280e0b1333f12d2f04aef90a36f268
hyo <kdhyo98@gmail.com>
2021-05-09 17:21:48 +0900

    계단오르기

--- python/백준/계단오르기.py
+++ python/백준/계단오르기.py
@@ -1,8 +1,15 @@
 # https://www.acmicpc.net/problem/2579
 
 n = int(input())
-step = []
+step = [0] * 300
 for i in range(n):
-    step.append(int(input()))
+    step[i] = int(input())
 
-print(step)
+dp = [0] * 300
+dp[0] = step[0]
+dp[1] = step[0] + step[1]
+dp[2] = max(step[2] + step[0], step[2] + step[1])
+for i in range(3, n):
+    dp[i] = max(step[i] + dp[i-2], step[i] + step[i-1] + dp[i-3])
+
+print(dp[n-1])
